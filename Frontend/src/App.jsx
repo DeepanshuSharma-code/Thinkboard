51b350c39d982adce714d386e0ca443a1d3050a9
Deepanshu Sharma <deepanshusharma06364@gmail.com>
2026-02-20 23:58:20 +0530

    fix: use react-router-dom instead of react-router

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Routes, Route } from 'react-router'
+import { Routes, Route } from 'react-router-dom'
 
 import Homepage from './Pages/HomePage.jsx'
 import CreatePage from './Pages/CreatePage.jsx'
